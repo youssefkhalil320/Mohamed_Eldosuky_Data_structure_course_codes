71fd165a49c7af1cdfa5312dc6bb23c0f4c1536f
youssefkhalil320 <youssefkhalil320@gmail.com>
2022-01-11 05:20:58 +0200

    linked-list-remaining-added

--- Linked_list/main.cpp
+++ Linked_list/main.cpp
@@ -4,101 +4,200 @@ using namespace std;
 class Node
 {
 public:
-    int data;
-    Node* next;
+	int data;
+	Node* next;
+	Node()
+	{
+		data = 0;
+		next = NULL;
+	}
 };
 
-class linkedList
+class Linkedlist
 {
 public:
-    Node* head;
-    linkedList()
-    {
-        head =NULL;
-    }
+	Node* head;
+	Linkedlist()
+	{
+		head = NULL;
+	}
 
-    bool is_empty()
-    {
-        return (head == NULL);
-    }
+	/*bool isempty()
+	{
+		if (head == NULL)
+			return true;
+		else
+			return false;
+	}*/
 
-   void insert_new(int value)
-{
-    Node* new_node;
-    new_node = new Node;
-    new_node->data = value; 
+	bool isempty()
+	{
+		return (head == NULL);
+	}
 
-    if(head == NULL)
-    {
-        head = new_node;
-        new_node-> next = NULL;   
-    }
-    else
-    {
-        Node* last = head;
-        while(last->next != NULL)
-        {
-            last = last->next;
-        }
-        last->next = new_node;
-        new_node->next = NULL;
-    }
-}
+	void InsertFirst(int newvalue)
+	{
+		Node* newnode= new Node();
+		newnode->data = newvalue;
+		if (isempty())
+		{
+			newnode->next = NULL;
+			head = newnode;
+		}
+		else
+		{
+			newnode->next = head;
+			head = newnode;
+		}
 
-     int counter()
-    {
-        int counter = 0;
-        Node* temp = head;
-        while(temp != NULL)
-        {
-            counter++;
-            temp = temp-> next;
-        }
-        return counter;
-    }
+	}
 
-    bool is_found(int key)
-    {
-        Node* temp = head;
-        bool flag = false;
-        while(temp != NULL)
-        {
-           if(temp-> data == key )
-           {
-               flag = true;
-           }
-           temp = temp ->next; 
-        }
-        return flag;
-    }
+	void display()
+	{
+		Node* temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}
 
-     void display()
-    {
-        Node* temp = head;
-        while(temp != NULL)
-        {
-            cout<<temp->data<<" ";
-            temp = temp-> next;
-        }
+	int count()
+	{
+		int counter=0;
+		Node* temp = head;
+		while (temp != NULL)
+		{
+			counter++;
+			temp = temp->next;
+		}
+		return counter;
+	}
 
-    }  
+	bool isfound(int key)
+	{
+		bool found = false;
+		Node* temp = head;
+		while (temp != NULL)
+		{
+			if (temp->data == key)
+				found = true;
+			temp = temp->next;
+		}
+		return found;
+
+	}
+	void insertbefore(int item, int newvalue)
+	{
+		if (isempty())
+			InsertFirst(newvalue);
+
+		if (isfound(item))
+		{
+			Node* newnode = new Node();
+			newnode->data = newvalue;
+			Node* temp = head;
+			while (temp != NULL && temp->next->data != item)
+			{
+				temp = temp->next;
+			}
+			newnode->next = temp->next;
+			temp->next = newnode;
+		}
+		else
+		{
+			cout << "Sorry , Item Not Found \n";
+		}
+	}
+	void append(int newvalue)
+	{
+		if (isempty())
+			InsertFirst(newvalue);
+		else
+		{
+		}
+		Node* temp = head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		Node* newnode = new Node();
+		newnode->data = newvalue;
+		temp->next = newnode;
+		newnode->next = NULL;
+	}
+
+	void Delete(int item)
+	{
+		
+		if (isempty())
+			cout << "List is empty , no items to delete \n";
+		Node* delptr = head;
+		if (head->data == item)
+		{
+			
+			head = head->next;
+			delete delptr;
+		}
+		else
+		{
+			Node* prev = NULL;
+			delptr = head;
+			while (delptr->data != item)
+			{
+				prev = delptr;
+				delptr = delptr->next;
+			}
+			prev->next = delptr->next;
+			delete delptr;
+
+		}
+
+		
+		
+		
+
+	}
+	
 };
+
 
 int main()
 {
-    linkedList my_list;
-    int new_element;
-    if(my_list.is_empty())
-    {
-        cout<<"The list is empty\n";
-    }
-    else
-    {
-        cout<<my_list.counter()<<endl;
-    }
-    cout<<"Please Enter a number to insert in the list"<<endl;
-    cin>>new_element;
-    my_list.insert_new(new_element);
-    my_list.display();
-    
-}                                                                                                                               
+	Linkedlist lst;
+	if (lst.isempty())
+		cout << "The List is Empty \n";
+	else
+		cout << "The List contains " << lst.count() <<endl;
+	int item;
+	cout << "Enter Item to insert in the list \n";
+	cin >> item;
+	lst.InsertFirst(item);
+	lst.display();
+	cout << "Enter Item to insert in the list \n";
+	cin >> item;
+	lst.InsertFirst(item);
+	lst.display();
+	cout << "Enter Item to insert in the list \n";
+	cin >> item;
+	lst.InsertFirst(item);
+	lst.display();
+	cout << "The List contains " << lst.count() << endl;
+
+	/*cout << "Enter Item to search for \n";
+	cin >> item;
+	if (lst.isfound(item))
+		cout << "Item Found \n";
+	else
+		cout << "Item not Found \n";*/
+
+	int newvalue;
+	cout << "Enter  item to delete \n";
+   cin >> item;
+	//cin >> newvalue;
+	/*lst.insertbefore(item, newvalue);*/
+	/*lst.append(newvalue);*/
+    lst.Delete(item);
+	lst.display();
+}
